5af773b81f1bb30fec5a3276959fc88f3c374c98
Roman Efimov <romaonthego@gmail.com>
2015-03-04 15:13:44 -0800

    Update storyboard

--- Example/Example/Classes/View Controllers/RootViewController.swift
+++ Example/Example/Classes/View Controllers/RootViewController.swift
@@ -21,18 +21,42 @@ class RootViewController: UITableViewController {
     //
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Add a section
         //
         self.manager.dataSource!.sections.append({
             let section = TableViewSection()
-            
             // Add items
             //
-            section.items = [TableViewItem(text: "Style", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
-                tableView.deselectRowAtIndexPath(indexPath, animated: true)
-                self.performSegueWithIdentifier("Root → Show Style", sender: nil)
-            })]
+            section.items = [
+                TableViewItem(text: "Forms", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Style", sender: nil)
+                }),
+                TableViewItem(text: "Forms", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Style", sender: nil)
+                }),
+                TableViewItem(text: "List", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Style", sender: nil)
+                }),
+                TableViewItem(text: "Editing", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Style", sender: nil)
+                }),
+                TableViewItem(text: "Retractable", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Style", sender: nil)
+                }),
+                TableViewItem(text: "Styling", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Style", sender: nil)
+                }),
+                TableViewItem(text: "Interface Builder Support", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Interface Builder Support", sender: nil)
+                }),
+                TableViewItem(text: "Indexed List", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Style", sender: nil)
+                }),
+                TableViewItem(text: "Validations", selectionHandler: { [unowned self] (section: TableViewSection, item: TableViewItem, tableView: UITableView, indexPath: NSIndexPath) in
+                    self.performSegueWithIdentifier("Root → Show Style", sender: nil)
+                }),
+            ]
             
             return section
         }())
