c610ad1fcd1591e2cfca011f809cf6b0c62942e6
Roman Efimov <romaonthego@gmail.com>
2015-03-04 11:19:59 -0800

    Add convenience methods to TableViewItem

--- TableViewManager/TableViewItem.swift
+++ TableViewManager/TableViewItem.swift
@@ -8,15 +8,76 @@
 
 import UIKit
 
+enum TableViewItemEditingStyle {
+    case None
+    case Delete
+    case Insert
+}
+
 class TableViewItem: NSObject, UIAccessibilityIdentification {
     
     var text: String?
     var detailText: String?
-    var configurationHandler: ((tableViewCell: TableViewCell) -> (Void))?
+    var section: TableViewSection?
+    var image: UIImage?
+    var highlightedImage: UIImage?
+    var editingStyle: TableViewItemEditingStyle = .None
+    var cellHeight: Float = 44
     var accessibilityIdentifier: String?
+    var configurationHandler: ((tableViewCell: TableViewCell) -> (Void))?
+    var selectionHandler: ((tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void))?
+    var insertionHandler: ((tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void))?
+    var deletionHandler: ((tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void))?
+    var deletionHandlerWithCompletion: ((tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath, (Void) -> (Void)) -> (Void))?
+    var moveHandler: ((tableView: UITableView, item: TableViewItem, sourceIndexPath: NSIndexPath, destinationIndexPath: NSIndexPath) -> (Bool))?
+    var moveCompletionHandler: ((tableView: UITableView, item: TableViewItem, sourceIndexPath: NSIndexPath, destinationIndexPath: NSIndexPath) -> (Void))?
+    var cutHandler: ((tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void))?
+    var copyHandler: ((tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void))?
+    var pasteHandler: ((tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void))?
     
-    init(text: String) {
+    convenience init(text: String) {
+        self.init()
         self.text = text
+    }
+    
+    convenience init(text: String, configurationHandler: (tableViewCell: TableViewCell) -> (Void)) {
+        self.init(text: text)
+        self.configurationHandler = configurationHandler
+    }
+    
+    convenience init(text: String, selectionHandler: (tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void)) {
+        self.init(text: text)
+        self.selectionHandler = selectionHandler
+    }
+    
+    convenience init(text: String, configurationHandler: (tableViewCell: TableViewCell) -> (Void), selectionHandler: (tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void)) {
+        self.init(text: text)
+        self.configurationHandler = configurationHandler
+        self.selectionHandler = selectionHandler
+    }
+    
+    convenience init(text: String, image: UIImage) {
+        self.init(text: text)
+        self.image = image
+    }
+    
+    convenience init(text: String, image: UIImage, configurationHandler: (tableViewCell: TableViewCell) -> (Void)) {
+        self.init(text: text)
+        self.image = image
+        self.configurationHandler = configurationHandler
+    }
+    
+    convenience init(text: String, image: UIImage, selectionHandler: (tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void)) {
+        self.init(text: text)
+        self.selectionHandler = selectionHandler
+        self.image = image
+    }
+    
+    convenience init(text: String, image: UIImage, configurationHandler: (tableViewCell: TableViewCell) -> (Void), selectionHandler: (tableView: UITableView, item: TableViewItem, indexPath: NSIndexPath) -> (Void)) {
+        self.init(text: text)
+        self.image = image
+        self.configurationHandler = configurationHandler
+        self.selectionHandler = selectionHandler
     }
     
 }
