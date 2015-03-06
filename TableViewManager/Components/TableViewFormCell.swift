ead82c1bf340a159f6774749684bb806d6588fb5
Roman Efimov <romaonthego@gmail.com>
2015-03-06 15:17:40 -0800

    Fix margin

--- TableViewManager/Components/TableViewFormCell.swift
+++ TableViewManager/Components/TableViewFormCell.swift
@@ -11,7 +11,18 @@ import UIKit
 class TableViewFormCell: TableViewCell {
 
     @IBOutlet weak var labelCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var labelRightMarginConstraint: NSLayoutConstraint!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    override func cellWillAppear() {
+        super.cellWillAppear()
+        self.titleLabel.text = self.item.text
+        if let text = self.item.text where count(text) > 0 {
+            self.labelRightMarginConstraint.constant = 5
+        } else {
+            self.labelRightMarginConstraint.constant = 0
+        }
+    }
     
     override func updateConstraints() {
         super.updateConstraints()
