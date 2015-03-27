105ec388aea13d52ae5e7794eb47f3aed2b599a2
Roman Efimov <romaonthego@gmail.com>
2015-03-26 23:08:46 -0700

    Add more items

---
+++ TableViewManager/Components/Date/TableViewDateCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewDateCell.swift
+//  Example
+//
+//  Created by Roman Efimov on 3/26/15.
+//  Copyright (c) 2015 Roman Efimov. All rights reserved.
+//
+
+import UIKit
+
+class TableViewDateCell: TableViewFormCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TableViewManager/Components/Picker/TableViewPickerItem.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewPickerItem.swift
+//  Example
+//
+//  Created by Roman Efimov on 3/26/15.
+//  Copyright (c) 2015 Roman Efimov. All rights reserved.
+//
+
+import UIKit
+
+class TableViewPickerItem: TableViewFormItem {
+   
+}
