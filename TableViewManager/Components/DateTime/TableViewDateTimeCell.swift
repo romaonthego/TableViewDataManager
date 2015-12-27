aaac85d70e59481431e2b7cb5289116b11362ab9
Roman Efimov <romaonthego@gmail.com>
2015-12-27 10:46:10 -0800

    Rename to date/time

--- TableViewManager/Components/DateTime/TableViewDateTimeCell.swift
+++ TableViewManager/Components/DateTime/TableViewDateTimeCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TableViewDateCell: TableViewFormCell {
+class TableViewDateTimeCell: TableViewFormCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
--- TableViewManager/Components/DateTime/TableViewDateTimeItem.swift
+++ TableViewManager/Components/DateTime/TableViewDateTimeItem.swift
@@ -8,6 +8,6 @@
 
 import UIKit
 
-class TableViewDateItem: TableViewFormItem {
+class TableViewDateTimeItem: TableViewFormItem {
    
 }
